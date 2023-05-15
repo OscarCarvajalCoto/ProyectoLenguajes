d9226acd912600abb1ebf078743213bbf3d7c0ce
OSCAR.CARVAJALCOTO@ucr.ac.cr <OSCAR.CARVAJALCOTO@ucr.ac.cr>
2023-05-15 11:33:40 -0600

    Oscar realizando prueba 2

--- ProyectoLenguajes/Controllers/HomeController.cs
+++ ProyectoLenguajes/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoLenguajes.Models;
 using System.Diagnostics;
-
+//Realizando una prueba para commitb
 namespace ProyectoLenguajes.Controllers
 {
     public class HomeController : Controller
