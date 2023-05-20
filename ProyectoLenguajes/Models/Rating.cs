86799ee7ff32b6889c0970574d58e9ab47ec6a73
David Aguilera <david28_12@hotmail.com>
2023-05-19 19:06:27 -0600

    Base conectada y modelos

---
+++ ProyectoLenguajes/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyectoLenguajes.Models;
+
+public partial class Rating
+{
+    public int rating_id { get; set; }
+
+    public int movies_series_id { get; set; }
+
+    public int user_id { get; set; }
+
+    public double rating1 { get; set; }
+}
