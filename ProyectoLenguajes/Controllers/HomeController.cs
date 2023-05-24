b5db01ce142852cc0034909fa49355cc8371c9be
OSCAR.CARVAJALCOTO@ucr.ac.cr <OSCAR.CARVAJALCOTO@ucr.ac.cr>
2023-05-23 22:30:18 -0600

    Actualizando Wilson

--- ProyectoLenguajes/Controllers/HomeController.cs
+++ ProyectoLenguajes/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoLenguajes.Models;
 using System.Diagnostics;
-//Prueba Wilson
+
 namespace ProyectoLenguajes.Controllers
 {
     public class HomeController : Controller
