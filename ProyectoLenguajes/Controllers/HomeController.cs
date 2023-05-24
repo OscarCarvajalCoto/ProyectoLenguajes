1f6331abdd7e671b78804d6f22531268b6f2dfb7
OSCAR.CARVAJALCOTO@ucr.ac.cr <OSCAR.CARVAJALCOTO@ucr.ac.cr>
2023-05-23 22:25:57 -0600

    Actualizando a 23-5-23 Master

--- ProyectoLenguajes/Controllers/HomeController.cs
+++ ProyectoLenguajes/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoLenguajes.Models;
 using System.Diagnostics;
-//Prueba David prueba git
-//Git
+
 namespace ProyectoLenguajes.Controllers
 {
     public class HomeController : Controller
