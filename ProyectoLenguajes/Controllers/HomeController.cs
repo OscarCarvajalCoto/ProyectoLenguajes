3402674a2a30592fbaba5a181de5ecf152ef52f8
OSCAR.CARVAJALCOTO@ucr.ac.cr <OSCAR.CARVAJALCOTO@ucr.ac.cr>
2023-05-23 22:32:13 -0600

    Actualizando Adrian

--- ProyectoLenguajes/Controllers/HomeController.cs
+++ ProyectoLenguajes/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoLenguajes.Models;
 using System.Diagnostics;
-//Prueba Adrian 4
+
 namespace ProyectoLenguajes.Controllers
 {
     public class HomeController : Controller
