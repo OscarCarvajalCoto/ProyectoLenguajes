163aa799ca520af5a27d55b2e156384dae8c8429
Wilson <wilsonbm11@gmail.com>
2023-06-06 18:56:33 -0600

    Camcios en el home page.

---
+++ ProyectoLenguajes/Models/HomePageModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoLenguajes.Models
+{
+    public class HomePageModel
+    {
+        public List<Movie_Serie> last_ten { get; set; }
+
+        public List<Movie_Serie> carrousel_1 { get; set; }
+
+        public List<Movie_Serie> carrousel_2 { get; set; }
+
+        public List<Movie_Serie> carrousel_3 { get; set; }
+    }
+}
